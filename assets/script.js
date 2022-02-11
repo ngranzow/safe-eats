c313bfb57d1dd6db012c6cc01b6956e6227ab679
ngranzow <nate.granzow@gmail.com>
2022-02-11 12:17:39 -0600

    updated drink recipe getelbyid

--- assets/script.js
+++ assets/script.js
@@ -18,41 +18,41 @@ function allRecipes() {
 
       console.log(yesRadio, textInput, mealTypeDrop, cuisineDrop, dietDrop, intChkStr);
 
-      // fetch(`https://api.spoonacular.com/recipes/search?query=${textInUS}&type=${mealTypeDrop}&cuisine=${cuisineDrop}&diet=${dietDrop}&intolerances=${intChkStr}&apiKey=4db28d341ddd49638dd20bb65bf0e98c`)
-      //     .then(function (foodResponse) {
-      //         return foodResponse.json();
-      //     })
-      //     .then(function (foodData) {
-      //         console.log(foodData);
-      //         for (const i = 0; i < foodData.results.length; i++) {
-      //             const recipesEl = document.getElementById("recipes");
-      //             const foodImage = foodData.results[i].image;
-      //             const foodTitle = foodData.results[i].title;
-      //             const foodTime = foodData.results[i].readyInMinutes;
-      //             const foodLink = foodData.results[i].sourceUrl;
+      fetch(`https://api.spoonacular.com/recipes/search?query=${textInUS}&type=${mealTypeDrop}&cuisine=${cuisineDrop}&diet=${dietDrop}&intolerances=${intChkStr}&apiKey=4db28d341ddd49638dd20bb65bf0e98c`)
+          .then(function (foodResponse) {
+              return foodResponse.json();
+          })
+          .then(function (foodData) {
+              console.log(foodData);
+              for (const i = 0; i < foodData.results.length; i++) {
+                  const recipesEl = document.getElementById("recipes");
+                  const foodImage = foodData.results[i].image;
+                  const foodTitle = foodData.results[i].title;
+                  const foodTime = foodData.results[i].readyInMinutes;
+                  const foodLink = foodData.results[i].sourceUrl;
 
-      //             console.log(foodImage, foodTitle, foodTime, foodLink)
+                  console.log(foodImage, foodTitle, foodTime, foodLink)
 
-      //             recipesEl.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='https://spoonacular.com/recipeImages/" + foodImage[i] + "' alt='" + foodTitle[i] + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + foodTitle[i] + "</h4><p><strong>Approximate time:</strong> " + foodTime[i] + "</p><button id='myBtn' class='button'>View Recipe</button></div></div></div></div>");
+                  recipesEl.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='https://spoonacular.com/recipeImages/" + foodImage[i] + "' alt='" + foodTitle[i] + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + foodTitle[i] + "</h4><p><strong>Approximate time:</strong> " + foodTime[i] + "</p><button id='myBtn' class='button'>View Recipe</button></div></div></div></div>");
 
-      //             function foodModal() {
-      //                 const modalBtn = document.getElementById("myBtn");
-      //                 modalBtn.addEventListener("click", function () {
-      //                     const modalContent = document.getElementById("modal-content");
-      //                     modalContent.classList.remove("modal");
-      //                     modalContent.innerHTML = ("<div class='card'><div class='card-content'><button type='reset' class='close button'>&times;</button></div></div>")
+                  function foodModal() {
+                      const modalBtn = document.getElementById("myBtn");
+                      modalBtn.addEventListener("click", function () {
+                          const modalContent = document.getElementById("modal-content");
+                          modalContent.classList.remove("modal");
+                          modalContent.innerHTML = ("<div class='card'><div class='card-content'><button type='reset' class='close button'>&times;</button></div></div>")
 
-      //                     const closeModalBtn = document.querySelector(".close");
-      //                     closeModalBtn.addEventListener("click", function () {
-      //                         modalContent.innerHTML = "";
-      //                         modalContent.classList.add("modal");
-      //                     })
-      //                 })
-      //             }
-      //           foodModal();
-      //         }
+                          const closeModalBtn = document.querySelector(".close");
+                          closeModalBtn.addEventListener("click", function () {
+                              modalContent.innerHTML = "";
+                              modalContent.classList.add("modal");
+                          })
+                      })
+                  }
+                foodModal();
+              }
 
-      //     })
+          })
 
       if (yesRadio) {
         const drinkApi = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
@@ -61,7 +61,7 @@ function allRecipes() {
             return drinkResponse.json();
           })
           .then(function (drinkData) {
-            const drinkRecipesEl = document.getElementById("recipes");
+            const drinkRecipesEl = document.getElementById("drinkRecipe");
             const drinkImage = drinkData.drinks[0].strDrinkThumb + "/preview";
             const drinkName = drinkData.drinks[0].strDrink;
             // var drinkInstructions = drinkData.drinks[0].strInstructions;
