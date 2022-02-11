4ffc3077c3f2a8110111642cf1a4a9bdb045fcd3
ngranzow <nate.granzow@gmail.com>
2022-02-11 12:14:31 -0600

    modal working

--- assets/script.js
+++ assets/script.js
@@ -18,115 +18,115 @@ function allRecipes() {
 
       console.log(yesRadio, textInput, mealTypeDrop, cuisineDrop, dietDrop, intChkStr);
 
-      fetch(`https://api.spoonacular.com/recipes/search?query=${textInUS}&type=${mealTypeDrop}&cuisine=${cuisineDrop}&diet=${dietDrop}&intolerances=${intChkStr}&apiKey=4db28d341ddd49638dd20bb65bf0e98c`)
-          .then(function (foodResponse) {
-              return foodResponse.json();
+      // fetch(`https://api.spoonacular.com/recipes/search?query=${textInUS}&type=${mealTypeDrop}&cuisine=${cuisineDrop}&diet=${dietDrop}&intolerances=${intChkStr}&apiKey=4db28d341ddd49638dd20bb65bf0e98c`)
+      //     .then(function (foodResponse) {
+      //         return foodResponse.json();
+      //     })
+      //     .then(function (foodData) {
+      //         console.log(foodData);
+      //         for (const i = 0; i < foodData.results.length; i++) {
+      //             const recipesEl = document.getElementById("recipes");
+      //             const foodImage = foodData.results[i].image;
+      //             const foodTitle = foodData.results[i].title;
+      //             const foodTime = foodData.results[i].readyInMinutes;
+      //             const foodLink = foodData.results[i].sourceUrl;
+
+      //             console.log(foodImage, foodTitle, foodTime, foodLink)
+
+      //             recipesEl.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='https://spoonacular.com/recipeImages/" + foodImage[i] + "' alt='" + foodTitle[i] + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + foodTitle[i] + "</h4><p><strong>Approximate time:</strong> " + foodTime[i] + "</p><button id='myBtn' class='button'>View Recipe</button></div></div></div></div>");
+
+      //             function foodModal() {
+      //                 const modalBtn = document.getElementById("myBtn");
+      //                 modalBtn.addEventListener("click", function () {
+      //                     const modalContent = document.getElementById("modal-content");
+      //                     modalContent.classList.remove("modal");
+      //                     modalContent.innerHTML = ("<div class='card'><div class='card-content'><button type='reset' class='close button'>&times;</button></div></div>")
+
+      //                     const closeModalBtn = document.querySelector(".close");
+      //                     closeModalBtn.addEventListener("click", function () {
+      //                         modalContent.innerHTML = "";
+      //                         modalContent.classList.add("modal");
+      //                     })
+      //                 })
+      //             }
+      //           foodModal();
+      //         }
+
+      //     })
+
+      if (yesRadio) {
+        const drinkApi = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+        fetch(drinkApi)
+          .then(function (drinkResponse) {
+            return drinkResponse.json();
           })
-          .then(function (foodData) {
-              console.log(foodData);
-              for (const i = 0; i < foodData.results.length; i++) {
-                  const recipesEl = document.getElementById("recipes");
-                  const foodImage = foodData.results[i].image;
-                  const foodTitle = foodData.results[i].title;
-                  const foodTime = foodData.results[i].readyInMinutes;
-                  const foodLink = foodData.results[i].sourceUrl;
-
-                  console.log(foodImage, foodTitle, foodTime, foodLink)
-
-                  recipesEl.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='https://spoonacular.com/recipeImages/" + foodImage[i] + "' alt='" + foodTitle[i] + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + foodTitle[i] + "</h4><p><strong>Approximate time:</strong> " + foodTime[i] + "</p><button id='myBtn' class='button'>View Recipe</button></div></div></div></div>");
-
-                  function foodModal() {
-                      const modalBtn = document.getElementById("myBtn");
-                      modalBtn.addEventListener("click", function () {
-                          const modalContent = document.getElementById("modal-content");
-                          modalContent.classList.remove("modal");
-                          modalContent.innerHTML = ("<div class='card'><div class='card-content'><button type='reset' class='close button'>&times;</button></div></div>")
-
-                          const closeModalBtn = document.querySelector(".close");
-                          closeModalBtn.addEventListener("click", function () {
-                              modalContent.innerHTML = "";
-                              modalContent.classList.add("modal");
-                          })
-                      })
-                  }
-              }
-              foodModal();
-
-          })
-
-          if (yesRadio) {
-              const drinkApi = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
-              fetch(drinkApi)
-                .then(function (drinkResponse) {
-                  return drinkResponse.json();
+          .then(function (drinkData) {
+            const drinkRecipesEl = document.getElementById("recipes");
+            const drinkImage = drinkData.drinks[0].strDrinkThumb + "/preview";
+            const drinkName = drinkData.drinks[0].strDrink;
+            // var drinkInstructions = drinkData.drinks[0].strInstructions;
+            const drinkIngredientArray = [drinkData.drinks[0].strIngredient1, drinkData.drinks[0].strIngredient2, drinkData.drinks[0].strIngredient3, drinkData.drinks[0].strIngredient4, drinkData.drinks[0].strIngredient5, drinkData.drinks[0].strIngredient6, drinkData.drinks[0].strIngredient7, drinkData.drinks[0].strIngredient8, drinkData.drinks[0].strIngredient9, drinkData.drinks[0].strIngredient10, drinkData.drinks[0].strIngredient11, drinkData.drinks[0].strIngredient12, drinkData.drinks[0].strIngredient13, drinkData.drinks[0].strIngredient14, drinkData.drinks[0].strIngredient15];
+            const drinkMeasureArray = [drinkData.drinks[0].strMeasure1, drinkData.drinks[0].strMeasure2, drinkData.drinks[0].strMeasure3, drinkData.drinks[0].strMeasure4, drinkData.drinks[0].strMeasure5, drinkData.drinks[0].strMeasure6, drinkData.drinks[0].strMeasure7, drinkData.drinks[0].strMeasure8, drinkData.drinks[0].strMeasure9, drinkData.drinks[0].strMeasure10, drinkData.drinks[0].strMeasure11, drinkData.drinks[0].strMeasure12, drinkData.drinks[0].strMeasure13, drinkData.drinks[0].strMeasure14, drinkData.drinks[0].strMeasure15];
+            const noNullIngredients = drinkIngredientArray.filter(x => x !== null);
+            const returnIngredients = noNullIngredients.join(", ");
+            console.log(noNullIngredients);
+            console.log(returnIngredients);
+            const noNullMeasure = drinkMeasureArray.filter(x => x !== null);
+            const returnMeasure = noNullMeasure.join(", ")
+            console.log(returnMeasure);
+  
+            drinkRecipesEl.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='" + drinkImage + "' alt='" + drinkName + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + drinkName + "</h4><button id='myBtn' class='button'>View Recipe</button></div></div></div></div>");
+  
+            function drinkModal() {
+              const modalBtn = document.getElementById("myBtn");
+              modalBtn.addEventListener("click", function () {
+                console.log("click");
+                const drinkImage = drinkData.drinks[0].strDrinkThumb + "/preview";
+                const drinkName = drinkData.drinks[0].strDrink;
+                const drinkInstructions = drinkData.drinks[0].strInstructions;
+                const drinkIngredientArray = [drinkData.drinks[0].strIngredient1, drinkData.drinks[0].strIngredient2, drinkData.drinks[0].strIngredient3, drinkData.drinks[0].strIngredient4, drinkData.drinks[0].strIngredient5, drinkData.drinks[0].strIngredient6, drinkData.drinks[0].strIngredient7, drinkData.drinks[0].strIngredient8, drinkData.drinks[0].strIngredient9, drinkData.drinks[0].strIngredient10, drinkData.drinks[0].strIngredient11, drinkData.drinks[0].strIngredient12, drinkData.drinks[0].strIngredient13, drinkData.drinks[0].strIngredient14, drinkData.drinks[0].strIngredient15];
+                const drinkMeasureArray = [drinkData.drinks[0].strMeasure1, drinkData.drinks[0].strMeasure2, drinkData.drinks[0].strMeasure3, drinkData.drinks[0].strMeasure4, drinkData.drinks[0].strMeasure5, drinkData.drinks[0].strMeasure6, drinkData.drinks[0].strMeasure7, drinkData.drinks[0].strMeasure8, drinkData.drinks[0].strMeasure9, drinkData.drinks[0].strMeasure10, drinkData.drinks[0].strMeasure11, drinkData.drinks[0].strMeasure12, drinkData.drinks[0].strMeasure13, drinkData.drinks[0].strMeasure14, drinkData.drinks[0].strMeasure15];
+                const noNullIngredients = drinkIngredientArray.filter(x => x !== null);
+                const returnIngredients = noNullIngredients.join(", ");
+                console.log(noNullIngredients);
+                console.log(returnIngredients);
+                const noNullMeasure = drinkMeasureArray.filter(x => x !== null);
+                const returnMeasure = noNullMeasure.join(", ")
+                console.log(returnMeasure);
+                const modalContent = document.getElementById("modal-content");
+                document.getElementById("myModal").style.display = "inline";
+                modalContent.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='" + drinkImage + "' alt='" + drinkName + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + drinkName + "</h4><br><strong>Ingredients:</strong><table id='ingredient-table'></table></p><p><strong>Directions:</strong> " + drinkInstructions + "</p></div></div></div></div></div>");
+  
+                const maxSize = Math.max(noNullIngredients.length, noNullMeasure.length);
+                const arrays = [];
+                for (var i = 0; i < maxSize; i++) {
+                  arrays[i] = [
+                    noNullIngredients[i] || "",
+                    noNullMeasure[i] || ""
+                  ];
+                }
+  
+                const columns = arrays.map(p => {
+                  const row = $("<tr>");
+                  p.forEach(v => row.append($("<td>").html(v)));
+                  return row;
                 })
-                .then(function (drinkData) {
-                  const drinkRecipesEl = document.getElementById("recipes");
-                  const drinkImage = drinkData.drinks[0].strDrinkThumb + "/preview";
-                  const drinkName = drinkData.drinks[0].strDrink;
-                  // var drinkInstructions = drinkData.drinks[0].strInstructions;
-                  const drinkIngredientArray = [drinkData.drinks[0].strIngredient1, drinkData.drinks[0].strIngredient2, drinkData.drinks[0].strIngredient3, drinkData.drinks[0].strIngredient4, drinkData.drinks[0].strIngredient5, drinkData.drinks[0].strIngredient6, drinkData.drinks[0].strIngredient7, drinkData.drinks[0].strIngredient8, drinkData.drinks[0].strIngredient9, drinkData.drinks[0].strIngredient10, drinkData.drinks[0].strIngredient11, drinkData.drinks[0].strIngredient12, drinkData.drinks[0].strIngredient13, drinkData.drinks[0].strIngredient14, drinkData.drinks[0].strIngredient15];
-                  const drinkMeasureArray = [drinkData.drinks[0].strMeasure1, drinkData.drinks[0].strMeasure2, drinkData.drinks[0].strMeasure3, drinkData.drinks[0].strMeasure4, drinkData.drinks[0].strMeasure5, drinkData.drinks[0].strMeasure6, drinkData.drinks[0].strMeasure7, drinkData.drinks[0].strMeasure8, drinkData.drinks[0].strMeasure9, drinkData.drinks[0].strMeasure10, drinkData.drinks[0].strMeasure11, drinkData.drinks[0].strMeasure12, drinkData.drinks[0].strMeasure13, drinkData.drinks[0].strMeasure14, drinkData.drinks[0].strMeasure15];
-                  const noNullIngredients = drinkIngredientArray.filter(x => x !== null);
-                  const returnIngredients = noNullIngredients.join(", ");
-                  console.log(noNullIngredients);
-                  console.log(returnIngredients);
-                  const noNullMeasure = drinkMeasureArray.filter(x => x !== null);
-                  const returnMeasure = noNullMeasure.join(", ")
-                  console.log(returnMeasure);
-        
-                  drinkRecipesEl.innerHTML = ("<div class='card'><div class='card-content'><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='" + drinkImage + "' alt='" + drinkName + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + drinkName + "</h4><button id='myBtn' class='button'>View Recipe</button></div></div></div></div>");
-        
-                  function drinkModal() {
-                    const modalBtn = document.getElementById("myBtn");
-                    modalBtn.addEventListener("click", function () {
-                      console.log("click");
-                      const drinkImage = drinkData.drinks[0].strDrinkThumb + "/preview";
-                      const drinkName = drinkData.drinks[0].strDrink;
-                      const drinkInstructions = drinkData.drinks[0].strInstructions;
-                      const drinkIngredientArray = [drinkData.drinks[0].strIngredient1, drinkData.drinks[0].strIngredient2, drinkData.drinks[0].strIngredient3, drinkData.drinks[0].strIngredient4, drinkData.drinks[0].strIngredient5, drinkData.drinks[0].strIngredient6, drinkData.drinks[0].strIngredient7, drinkData.drinks[0].strIngredient8, drinkData.drinks[0].strIngredient9, drinkData.drinks[0].strIngredient10, drinkData.drinks[0].strIngredient11, drinkData.drinks[0].strIngredient12, drinkData.drinks[0].strIngredient13, drinkData.drinks[0].strIngredient14, drinkData.drinks[0].strIngredient15];
-                      const drinkMeasureArray = [drinkData.drinks[0].strMeasure1, drinkData.drinks[0].strMeasure2, drinkData.drinks[0].strMeasure3, drinkData.drinks[0].strMeasure4, drinkData.drinks[0].strMeasure5, drinkData.drinks[0].strMeasure6, drinkData.drinks[0].strMeasure7, drinkData.drinks[0].strMeasure8, drinkData.drinks[0].strMeasure9, drinkData.drinks[0].strMeasure10, drinkData.drinks[0].strMeasure11, drinkData.drinks[0].strMeasure12, drinkData.drinks[0].strMeasure13, drinkData.drinks[0].strMeasure14, drinkData.drinks[0].strMeasure15];
-                      const noNullIngredients = drinkIngredientArray.filter(x => x !== null);
-                      const returnIngredients = noNullIngredients.join(", ");
-                      console.log(noNullIngredients);
-                      console.log(returnIngredients);
-                      const noNullMeasure = drinkMeasureArray.filter(x => x !== null);
-                      const returnMeasure = noNullMeasure.join(", ")
-                      console.log(returnMeasure);
-                      const modalContent = document.getElementById("modal-content");
-                      modalContent.classList.remove("modal");
-                      modalContent.innerHTML = ("<div class='card'><div class='card-content'><button type='reset' class='close button'>&times;</button><div class='media'><div class='media-left'><figure class'image is-32x32'><img src='" + drinkImage + "' alt='" + drinkName + "'></figure></div><div class='media-content'><div class='content'><p><h4>" + drinkName + "</h4><br><strong>Ingredients:</strong><table id='ingredient-table'></table></p><p><strong>Directions:</strong> " + drinkInstructions + "</p></div></div></div></div></div>");
-        
-                      const maxSize = Math.max(noNullIngredients.length, noNullMeasure.length);
-                      const arrays = [];
-                      for (var i = 0; i < maxSize; i++) {
-                        arrays[i] = [
-                          noNullIngredients[i] || "",
-                          noNullMeasure[i] || ""
-                        ];
-                      }
-        
-                      const columns = arrays.map(p => {
-                        const row = $("<tr>");
-                        p.forEach(v => row.append($("<td>").html(v)));
-                        return row;
-                      })
-        
-                      $("#ingredient-table").append(columns);
-        
-                        const closeModalBtn = document.querySelector(".close");
-                        closeModalBtn.addEventListener("click", function() {
-                          modalContent.innerHTML = "";
-                          modalContent.classList.add("modal");
-                        })
-                    
-                    })
-        
-                  }
-        
-                  drinkModal();
-                })
+  
+                $("#ingredient-table").append(columns);
+  
+                  const closeModalBtn = document.querySelector(".close");
+                  closeModalBtn.addEventListener("click", function() {
+                    modalContent.innerHTML = "";
+                    document.getElementById("myModal").style.display = "none";
+                  })
+              
+              })
+  
             }
+  
+            drinkModal();
+          })
+      }
   })
 }
 
